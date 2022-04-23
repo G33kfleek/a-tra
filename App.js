d38a5a1d64d09b06aab682c42ff32b8c7f8637d5
G33kfleek <radrex460@gmail.com>
2022-04-23 21:21:50 +0300

    test

--- App.js
+++ App.js
@@ -1,28 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert,Button } from 'react-native';
 const Mpesa = require('mpesa-node')
 const mpesaApi = new Mpesa({ consumerKey: 'PMF2qGOtr5aYyJGeJrhIi6V8o4eq0QxC', consumerSecret: 'CcA7IeLMuERtSBJQ' })
 // another instance
 // const instance = new Mpesa({ consumerKey: 'test', consumerSecret: 'test', environment: 'production' })
 const { shortCode } = mpesaApi.configs
 var m_node = 2
-mpesaApi
-    .c2bSimulate(
-        254798362712,
-        500,
-        'h6dk0Ue2'
-    )
-    .then((result) => {
-        m_node = 1
-    })
-    .catch((err) => {
-        // retry?
-    })
+
+
+
+
+
+
+
+
+
 export default function App() {
+
+  const createTwoButtonAlert = () =>
+  Alert.alert(
+    "Alert Title",
+    "My Alert Msg",
+    [
+      {
+        text: "Cancel",
+        onPress: () => console.log("Cancel Pressed"),
+        style: "cancel"
+      },
+      { text: "OK", onPress: () => console.log("OK Pressed") }
+    ]
+  );
   return (
     <View style={styles.container}>
+        <View style={styles.button_test}>
+          <Button title={"2-Button Alert"} onPress={createTwoButtonAlert} />
+       </View>
       <Text>
-        mpesaApi
+        mpesaApiss
         text{m_node}</Text>
       <StatusBar style="auto" />
     </View>
@@ -35,5 +49,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#efff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+    button_test: {
+      flex: 1,
+      justifyContent: "space-around",
+      alignItems: "center"
   },
 });
